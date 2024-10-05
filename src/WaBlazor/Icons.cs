ecb9429019f363eef0ae5525691a7710d5518553
Ola Bäcker <olabacker@gmail.com>
2024-10-05 15:10:11 +0200

    added checkbox

---
+++ src/WaBlazor/Icons.cs
@@ -0,0 +1,19 @@
+﻿namespace WaBlazor;
+
+public enum FaType
+{
+    Solid,
+    Regular,
+    Light,
+    Duotone,
+    Brand
+}
+
+public enum WaVariant
+{
+    Brand,
+    Success,
+    Neutral,
+    Warning,
+    Danger
+}
